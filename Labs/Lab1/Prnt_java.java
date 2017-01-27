6b5fb2294b64aa4f168e9410376f84d6a2732b39
Paul <paul.ippolito2@marist.edu>
2017-01-27 17:18:51 -0500

    Solution to exercise 1.3 for Lab 1.

---
+++ Labs/Lab1/Prnt_java.java
@@ -0,0 +1,14 @@
+// Paul Ippolito
+// CMPT220
+// 1/27/17
+// This program displays the word Java completely made out
+// of the leters that spell Java
+public class Prnt_java {
+  public static void main(String[] args) {
+	System.out.println("    J     A      V     V      A");
+	System.out.println("    J   A   A     V   V     A   A");
+	System.out.println(" J  J  AAAAAAA     V V     AAAAAAA");
+	System.out.println("  JJ  A       A     V     A       A");
+  	
+  }
+}
